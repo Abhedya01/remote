ce25f7669110da8ea66293271f76bd33e16c9a34
roHIT <rohitksh2000@gmail.com>
2024-09-03 17:31:50 +0530

    changes

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 function myFunk() {
-  console.log("this is function");
+    console.log("this is function");
 }
 const ahe = 25;
-const hyena = 56;
-const isAdult = () => {
-    if (age > 18) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
